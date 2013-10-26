aa08730754c2ba123923f073c0fcc0729f56d847
Christian Jaeger <ch@christianjaeger.ch>
2013-10-26 20:19:37 +0100

    choose GPU instead

--- main.c
+++ main.c
@@ -40,7 +40,7 @@ int main(void) {
     cl_uint ret_num_devices;
     cl_uint ret_num_platforms;
     cl_int ret = clGetPlatformIDs(1, &platform_id, &ret_num_platforms);
-    ret = clGetDeviceIDs( platform_id, CL_DEVICE_TYPE_CPU, 1, 
+    ret = clGetDeviceIDs( platform_id, CL_DEVICE_TYPE_GPU, 1, 
             &device_id, &ret_num_devices);
 
     // Create an OpenCL context
