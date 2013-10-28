6930303ac0483d4c6c7d68b34dbfe9876fa70500
Christian Jaeger <ch@christianjaeger.ch>
2013-10-28 08:16:12 +0000

    fix segfault, of course

--- main.c
+++ main.c
@@ -53,7 +53,7 @@ int main(void) {
     printf("platform_id=%p, ret_num_platforms=%i\n",platform_id, ret_num_platforms);
     ret = clGetDeviceIDs( platform_id, CL_DEVICE_TYPE_GPU, 1, 
             &device_id, &ret_num_devices);
-    CHECKRET ("clGetDeviceIDs",err10);
+    CHECKRET ("clGetDeviceIDs",err0); // XXX deallocate platform_id ?
 
     // Create an OpenCL context
     cl_context context = clCreateContext( NULL, 1, &device_id, NULL, NULL, &ret);
